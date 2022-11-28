8a771c46462ec17fc8f744758d72476895957207
BABI <elhabibbabi@gmail.com>
2022-11-28 17:01:06 +0100

    Premier push!

---
+++ MVVN_WPF/Model/User.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace MVVN_WPF.Model
+{
+    public class User:INotifyPropertyChanged
+    {
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+        private string _city;
+        private string _state;
+        private string _country;
+        
+        public int Id 
+        {
+            get{return _id;}
+            set{_id = value; OnPropertyChanged("_id");}
+        }
+        public string FirstName
+        {
+            get{return _firstName;} 
+            set{_firstName = value; OnPropertyChanged("FirstName");} 
+        }
+        public string LastName 
+        {
+            get{return _lastName;}
+            set{_lastName = value; OnPropertyChanged("LastName");}
+        }
+        public string City 
+        {
+            get { return _city; }
+            set { _city = value; OnPropertyChanged("City"); }
+        }
+        public string State 
+        {
+            get { return _state; }
+            set { _state = value; OnPropertyChanged("State"); }
+        }
+        public string Country 
+        {
+            get { return _country; }
+            set { _country = value;OnPropertyChanged("Country"); }
+        }
+
+        #region INotifyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
---
+++ MVVN_WPF/ViewModel/UserViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using System.ComponentModel;
+using MVVN_WPF.Model;
+using System.Data;
+
+namespace MVVN_WPF.ViewModel
+{
+     class UserViewModel
+    {
+        private IList<User> _users;
+        public UserViewModel() 
+        { 
+            _users = new List<User>
+            {
+                new User { Id = 1, FirstName = "NANA", LastName = "SANI", City = "DAKAR", State = "SN", Country = "SENEGAL" },
+                new User { Id = 2, FirstName = "NANA", LastName = "SANI", City = "DAKAR", State = "SN", Country = "SENEGAL" },
+                new User { Id = 3, FirstName = "NANA", LastName = "SANI", City = "DAKAR", State = "SN", Country = "SENEGAL"}
+            };
+        }
+        public IList<User> users 
+        { 
+            get { return _users; } 
+            set { _users = value; }
+        }
+        private ICommand _mUpdater;
+        public ICommand UpdateCommand
+        {
+            get
+            {
+                if (_mUpdater == null)
+                    _mUpdater = new Updater();
+                return _mUpdater;
+            }
+            set { _mUpdater = value; }
+        }
+        private class Updater : ICommand 
+        {
+            #region Icommand Members
+            public bool CanExecute(object parameter) 
+            {
+                return true;
+            }
+            public event EventHandler CanExecuteChanged;
+            public void Execute(object parameter) 
+            {
+            }
+            #endregion
+        }
+    }
+}
